e31e5c6b1535cb4a87addbcf0a457acc26e63afe
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-09-12 14:49:19 +0330

    change CustomUserDetailsService for do that single responsibilities

--- src/main/java/com/example/finalproject_phase2/securityConfig/CustomUserDetailsService.java
+++ src/main/java/com/example/finalproject_phase2/securityConfig/CustomUserDetailsService.java
@@ -3,11 +3,11 @@ package com.example.finalproject_phase2.securityConfig;
 import com.example.finalproject_phase2.entity.Admin;
 import com.example.finalproject_phase2.entity.Customer;
 import com.example.finalproject_phase2.entity.Specialist;
-import com.example.finalproject_phase2.repository.AdminRepository;
-import com.example.finalproject_phase2.repository.CustomerRepository;
-import com.example.finalproject_phase2.repository.SpecialistRepository;
+import com.example.finalproject_phase2.service.AdminService;
+import com.example.finalproject_phase2.service.CustomerService;
+import com.example.finalproject_phase2.service.SpecialistService;
 import com.example.finalproject_phase2.util.CheckValidation;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,38 +15,52 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final AdminRepository adminRepository;
-    private final SpecialistRepository specialistRepository;
-    private final CustomerRepository customerRepository;
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (CheckValidation.userType.equals("admin")) {
-            Optional<Admin> userOptional = adminRepository.findByEmail(username.trim());
-            if (userOptional.isPresent()) {
-                return new AdminUserDetail(
-                        userOptional.get()
-                );
-            }
-            throw new UsernameNotFoundException(username + " not found");
-        }
-    else if(CheckValidation.userType.equals("customer")){
-        Optional<Customer> userOptional = customerRepository.findByEmail(username.trim());
-        if (userOptional.isPresent()) {
-            return new CustomerUserDetail(
-                    userOptional.get()
-            );
-        }
-        throw new UsernameNotFoundException(username + " not found");
-    }else {
-            Optional<Specialist> userOptional = specialistRepository.findByEmail(username.trim());
-            if (userOptional.isPresent()) {
-                return new SpecialistUserDetail(
-                        userOptional.get()
-                );
-            }
-            throw new UsernameNotFoundException(username + " not found");
-        }
+  //  private final AdminRepository adminRepository;
+  //  private final SpecialistRepository specialistRepository;
+  // private final CustomerRepository customerRepository;
+  private final SpecialistService specialistService;
+    private final AdminService adminService;
+    private final CustomerService customerService;
+
+    public CustomUserDetailsService(@Lazy SpecialistService specialistService, @Lazy AdminService adminService, @Lazy CustomerService customerService) {
+        this.specialistService = specialistService;
+        this.adminService = adminService;
+        this.customerService = customerService;
     }
+
+    @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      if (CheckValidation.userType.equals("admin")) {
+          // Optional<Admin> userOptional = adminRepository.findByEmail(username.trim());
+          Optional<Admin> userOptional = adminService.findByEmail(username.trim());
+          if (userOptional.isPresent()) {
+              return new AdminUserDetail(
+                      userOptional.get()
+              );
+          }
+          throw new UsernameNotFoundException(username + " not found");
+      }
+      else if(CheckValidation.userType.equals("customer")){
+          //  Optional<Customer> userOptional = customerRepository.findByEmail(username.trim());
+          Optional<Customer> userOptional = customerService.findByEmail(username.trim());
+          if (userOptional.isPresent()) {
+              return new CustomerUserDetail(
+                      userOptional.get()
+              );
+          }
+          throw new UsernameNotFoundException(username + " not found");
+      }else {
+          //     Optional<Specialist> userOptional = specialistRepository.findByEmail(username.trim());
+          Optional<Specialist> userOptional = specialistService.findByEmailOptional(username.trim());
+          if (userOptional.isPresent()) {
+              return new SpecialistUserDetail(
+                      userOptional.get()
+              );
+          }
+          throw new UsernameNotFoundException(username + " not found");
+      }
+  }
+
 }
